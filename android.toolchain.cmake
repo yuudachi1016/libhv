73cea93348add0f6eecf0ab5bcb798de37b31aa7
hongxin.zhang <hongxin.zhang@ysqxds.cn>
2023-11-03 16:38:51 +0800

    更新Android编译设置

--- android.toolchain.cmake
+++ android.toolchain.cmake
@@ -2,12 +2,12 @@ set(CMAKE_SYSTEM_NAME Android)
 
 #set(CMAKE_SYSTEM_VERSION 21)
 
-set(CMAKE_ANDROID_API 30)
+set(CMAKE_ANDROID_API 24)
 
 set(ANDROID_ABI "arm64-v8a")
 
 set(CMAKE_ANDROID_NDK /home/hongxin/android-ndk-r21e)
 
-set(CMAKE_ANDROID_STL_TYPE gnustl_static)
+set(CMAKE_ANDROID_STL_TYPE c++_static)
 
 include(/home/hongxin/android-ndk-r21e/build/cmake/android.toolchain.cmake)
